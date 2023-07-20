edcf24e1feee30ab08ce714836c524657bd7b622
chandana444 <72591703+chandana444@users.noreply.github.com>
2023-07-20 21:55:14 +0530

    Create Reverse Words in a String.py

---
+++ Array_or_String/Reverse Words in a String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s=s.split()
+        res=[]
+        for i in range(len(s)-1,-1,-1):
+            res.append(s[i])
+        res=" ".join(res)
+        return res
