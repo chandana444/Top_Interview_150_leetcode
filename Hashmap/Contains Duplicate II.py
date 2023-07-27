03831a35e47e8e367d290bd2ab2991e0d2e06299
chandana444 <72591703+chandana444@users.noreply.github.com>
2023-07-27 08:24:15 +0530

    Create Contains Duplicate II.py

---
+++ Hashmap/Contains Duplicate II.py
@@ -0,0 +1,11 @@
+import numpy as np
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        res={}
+        for i in range(len(nums)):
+            if nums[i] in res and abs(i-res[nums[i]])<=k:
+                return True
+            res[nums[i]]=i
+            #print(res)
+        return False
+             
